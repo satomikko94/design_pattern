53536b3cb7306599e565726155c4b6029395ff35
satomikko94 <satomikko94@gmail.com>
2014-05-08 09:05:53 +0900

    create adapter pattern

---
+++ 2.Adapter/src/PrintBanner.java
@@ -0,0 +1,18 @@
+
+public class PrintBanner extends Banner implements Print {
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeek() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        showWithAster();
+    }
+
+}
---
+++ 2.Adapter2/src/PrintBanner.java
@@ -0,0 +1,18 @@
+
+public class PrintBanner extends Print {
+    private Banner banner;
+    public PrintBanner(String string) {
+        this.banner = new Banner(string);
+    }
+
+    @Override
+    public void printWeek() {
+        banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster();
+    }
+
+}
