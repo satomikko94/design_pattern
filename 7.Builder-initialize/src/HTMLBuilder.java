622c7f9097b137c6b10f4bfa14d54cd22cafaa1f
satomikko94 <satomikko94@gmail.com>
2014-05-28 09:01:06 +0900

    P94 7-2 add initialized check

---
+++ 7.Builder-initialize/src/HTMLBuilder.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class HTMLBuilder extends Builder {
+    private String filename;
+    private PrintWriter writer;
+    @Override
+    public void buildTitle(String title) {
+        filename = title + ".html";
+        try {
+            writer = new PrintWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer.println("<html><head><title>" + title + "</title></head><body>");
+        writer.println("<h1>" + title + "</h1>");
+    }
+
+    @Override
+    public void buildString(String str) {
+        writer.println("<p>" + str + "</p>");
+    }
+
+    @Override
+    public void buildItems(String[] items) {
+        writer.println("<ul>");
+        for (int i = 0; i < items.length; i++) {
+            writer.println("<li>" + items[i] + "</li>");
+        }
+        writer.println("</ul>");
+    }
+
+    @Override
+    public void buildDone() {
+        writer.println("</body></html>");
+        writer.close();
+    }
+    
+    public String getResult() {
+        return filename;
+    }
+
+}
